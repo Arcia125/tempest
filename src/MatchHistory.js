dde5e69c806c91777ee92d345c0f6d5cb0394f51
Kevin Hallett <kevinmhallett@gmail.com>
2020-05-26 21:01:02 -0500

    add match history

---
+++ src/MatchHistory.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const MatchHistory = props => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+MatchHistory.propTypes = {
+
+}
+
+export default MatchHistory
--- src/Profile.js
+++ src/Profile.js
@@ -2,12 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import UsernamePlate from './UsernamePlate'
 import RankedBadge from './RankedBadge'
+import MatchHistory from './MatchHistory'
 
 const Profile = props => {
   return (
     <div>
       <UsernamePlate username="Arcia125"/>
       <RankedBadge league="Ranked Solo" rank="Silver 4" />  
+      <MatchHistory />
     </div>
   )
 }
