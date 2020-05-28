518a4e742526782491d1f123e6539106ddce8df8
Kevin Hallett <kevinmhallett@gmail.com>
2020-05-28 11:01:54 -0500

    add icon
    change window title

--- public/electron.js
+++ public/electron.js
@@ -2,11 +2,13 @@ const path = require('path');
 const { app, BrowserWindow } = require('electron');
 const isDev = require('electron-is-dev');
 
+const iconUrl = path.resolve(__dirname, 'favicon.ico');
+
 let mainWindow;
 
 const indexHtmlPath = path.join(__dirname, './build/index.html');
 const indexHtmlUrl = `file://${indexHtmlPath}`;
-const browserWindowOpts = { width: 800, height: 600 };
+const browserWindowOpts = { width: 800, height: 600, icon: iconUrl };
 
 function createWindow() {
   // Create the browser window.
@@ -14,15 +16,15 @@ function createWindow() {
   // and load the index.html of the app.     win.loadFile('index.html')
   // mainWindow.loadURL('http://localhost:3000');
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : indexHtmlUrl);
-  mainWindow.on('closed', () => mainWindow = null);
+  mainWindow.on('closed', () => (mainWindow = null));
 }
 
 app.on('ready', createWindow);
 
 app.on('activate', () => {
-  if(mainWindow === null) createWindow(); 
-})
+  if (mainWindow === null) createWindow();
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
-})
+});
