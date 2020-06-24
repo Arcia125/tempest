7d615ce6f22cd777f5c815f163a1bd478714c942
Kevin Hallet <kevinmhallett@gmail.com>
2020-06-24 09:04:09 -0500

    add lcu-connector

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { app, BrowserWindow, nativeImage } = require('electron');
 const isDev = require('electron-is-dev');
+const LCUConnector = require('lcu-connector');
 
 const iconUrl = path.resolve(__dirname, 'favicon.ico');
 
@@ -8,6 +9,7 @@ let mainWindow;
 
 const indexHtmlPath = path.join(__dirname, './build/index.html');
 const indexHtmlUrl = `file://${indexHtmlPath}`;
+const lcuConnector = new LCUConnector();
 
 function createWindow() {
   // Create the browser window.
@@ -21,6 +23,7 @@ function createWindow() {
   // mainWindow.loadURL('http://localhost:3000');
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : indexHtmlUrl);
   mainWindow.on('closed', () => (mainWindow = null));
+  lcuConnector.start();
 }
 
 app.whenReady().then(createWindow);
@@ -31,4 +34,9 @@ app.on('activate', () => {
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
+});
+
+lcuConnector.on('connect', async (data) => {
+  console.log(data);
+  mainWindow.webContents.send('lcu-data', data);
 });
