ac834c666f678394647f4dc202830881d59d91ac
Kevin Hallett <kevinmhallett@gmail.com>
2020-07-02 17:56:41 -0500

    add MatchMakingResponse

--- src/shared/LCUResponses.ts
+++ src/shared/LCUResponses.ts
@@ -19,3 +19,38 @@ export interface CurrentSummonerResponse {
   xpSinceLastLevel: number;
   xpUntilNextLevel: number;
 }
+
+export interface MatchMakingResponse {
+  dodgeData: DodgeData;
+  errors: any[];
+  estimatedQueueTime: number;
+  isCurrentlyInQueue: boolean;
+  lobbyId: string;
+  lowPriorityData: LowPriorityData;
+  queueId: number;
+  readyCheck: ReadyCheck;
+  searchState: string;
+  timeInQueue: number;
+}
+
+export interface DodgeData {
+  dodgerId: number;
+  state: string;
+}
+
+export interface LowPriorityData {
+  bustedLeaverAccessToken: string;
+  penalizedSummonerIds: any[];
+  penaltyTime: number;
+  penaltyTimeRemaining: number;
+  reason: string;
+}
+
+export interface ReadyCheck {
+  declinerIds: any[];
+  dodgeWarning: string;
+  playerResponse: string;
+  state: string;
+  suppressUx: boolean;
+  timer: number;
+}
