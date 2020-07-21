55f78864e00c04d87785a0674c415af881594d89
Kevin Hallett <kevinmhallett@gmail.com>
2020-07-21 12:42:13 -0500

    use Typography for Legal

--- src/components/Legal.tsx
+++ src/components/Legal.tsx
@@ -1,15 +1,19 @@
 import React, { FC, HTMLAttributes } from 'react';
 import './Legal.css';
+import Typography, { TypographyVariants } from './Typography';
 import { classNames } from '../utils';
 
 export type Props = HTMLAttributes<HTMLParagraphElement>;
 
 export const Legal: FC<Props> = ({ className }) => {
   return (
-    <p className={classNames('Legal', className)}>
+    <Typography
+      className={classNames('Legal', className)}
+      variant={TypographyVariants.p}
+    >
       Tempest was created under Riot Games' "Legal Jibber Jabber" policy using
       assets owned by Riot Games. Riot Games does not endorse or sponsor this
       project.
-    </p>
+    </Typography>
   );
 };
