ce90b493207e4ebdb53f2fc02a98ed1f325d2218
Kevin Hallett <kevinmhallett@gmail.com>
2020-07-28 08:55:17 -0500

    fix null check

--- src/hooks/useCurrentSummoner.ts
+++ src/hooks/useCurrentSummoner.ts
@@ -64,7 +64,7 @@ export const useCurrentSummoner = () => {
         actions.err(dispatch, e);
       }
     }
-    if ('username' in lcuData) {
+    if (lcuData != null) {
       log.silly('getting summoner');
       getSummoner();
     }
