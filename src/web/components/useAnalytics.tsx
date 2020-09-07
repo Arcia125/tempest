f1f37de6d716680b8becd75832953d5002d15d26
Kevin Hallett <kevinmhallett@gmail.com>
2020-09-07 14:49:13 -0500

    add analytics debugging

--- src/web/components/useAnalytics.tsx
+++ src/web/components/useAnalytics.tsx
@@ -8,7 +8,9 @@ const useAnalyticsPageListener = () => {
   const location = useLocation();
   useEffect(() => {
     if (process.env.REACT_APP_GA_TRACKING_ID && !analyticsInitialized) {
-      ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+      ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID, {
+        debug: !!process.env.REACT_APP_GA_DEBUG,
+      });
       analyticsInitialized = true;
     }
     if (analyticsInitialized) {
