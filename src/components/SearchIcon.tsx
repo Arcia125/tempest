9eaf1b9e0188c5030695d52080ed6b6e1bc34a40
Kevin Hallett <kevinmhallett@gmail.com>
2020-09-12 12:50:19 -0500

    update search icon appearance

--- src/components/SearchIcon.tsx
+++ src/components/SearchIcon.tsx
@@ -1,7 +1,6 @@
 import React, { FC, SVGProps, useContext } from 'react';
 import { themeContext } from '../theme';
 
-
 interface BaseProps {}
 
 type Props = SVGProps<SVGSVGElement> & BaseProps;
@@ -9,27 +8,11 @@ type Props = SVGProps<SVGSVGElement> & BaseProps;
 const SearchIcon: FC<Props> = (props) => {
   const { theme } = useContext(themeContext);
   return (
-    <svg width={27} height={25} fill="none" {...props}>
-      <defs>
-        <circle id="outer-mag" cx={10.5} cy={10.5} r={10.5} />
-        <clipPath id="clip">
-          <use xlinkHref="#outer-mag" />
-        </clipPath>
-      </defs>
-      <g>
-        <use
-          xlinkHref="#outer-mag"
-          stroke={theme.colors.accent0}
-          strokeWidth="6"
-          clipPath="url(#clip)"
-        />
-      </g>
+    <svg width={29} height={30} fill="none" {...props}>
       <path
-        stroke={theme.colors.accent0}
-        strokeWidth={3}
-        d="M7.061 5.939l18 18"
+        d="M18.73 17.5h-.955l-.339-.337a8.254 8.254 0 001.897-5.288c0-4.487-3.516-8.125-7.854-8.125s-7.854 3.638-7.854 8.125c0 4.488 3.516 8.125 7.854 8.125a7.669 7.669 0 005.111-1.962l.327.35v.987l6.041 6.238 1.8-1.863-6.029-6.25zm-7.25 0c-3.01 0-5.438-2.512-5.438-5.625S8.47 6.25 11.479 6.25c3.009 0 5.438 2.512 5.438 5.625s-2.43 5.625-5.438 5.625z"
+        fill={theme.colors.accent0}
       />
-      <circle cx={10.5} cy={10.5} r={8.5} fill={theme.colors.background} />
     </svg>
   );
 };
