bbada2870f15470724d6457121d7165d0da12e7e
Kevin Hallett <kevinmhallett@gmail.com>
2020-09-22 11:59:04 -0500

    adjust apollo client uri

--- src/apolloClient.tsx
+++ src/apolloClient.tsx
@@ -1,5 +1,5 @@
 import ApolloClient from 'apollo-boost';
 
 export const apolloClient = new ApolloClient({
-  uri: process.env.REACT_APP_API_URL,
+  uri: process.env.REACT_APP_API_URL || 'https://lol-maelstrom.herokuapp.com/',
 });
