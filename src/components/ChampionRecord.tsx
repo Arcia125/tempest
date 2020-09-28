342320e15550dc933302e3944a947146c0b617a8
Kevin Hallett <kevinmhallett@gmail.com>
2020-09-28 16:21:15 -0500

    add champion name to champion record

--- src/components/ChampionRecord.tsx
+++ src/components/ChampionRecord.tsx
@@ -25,40 +25,48 @@ export const ChampionRecord: FC<Props> = ({ champRecord }) => {
     champRecord.win === 0
       ? 0
       : (champRecord.win / (champRecord.win + champRecord.loss)) * 100;
+  const championName = getChampionName(champRecord.key);
   return (
     <div className="ChampionRecord" key={champRecord.key}>
       <RiotImage
         className="ChampionRecord-champion-image"
-        alt={getChampionName(champRecord.key)}
+        alt={championName}
         type={RiotImageType.CHAMPION}
         name={getImageNameByChampionKey(String(champRecord.key))}
       />
       <div className="ChampionRecord-info">
-        <Typography
-          className="ChampionRecord-win-pct"
-          variant={TypographyVariants.p}
-          color={winPctColorRange.interpolate(winPct)}
-        >
-          {winPct}%
-        </Typography>
-        <Typography
-          className="ChampionRecord-win-count"
-          variant={TypographyVariants.xs}
-        >
-          {champRecord.win}W
-        </Typography>
-        <Typography
-          className="ChampionRecord-divider"
-          variant={TypographyVariants.xs}
-        >
-          /
-        </Typography>
-        <Typography
-          className="ChampionRecord-loss-count"
-          variant={TypographyVariants.xs}
-        >
-          {champRecord.loss}L
-        </Typography>
+        <div className="ChampionRecord-info-top">
+          <Typography fontWeight="600" variant={TypographyVariants.p}>
+            {championName}
+          </Typography>
+        </div>
+        <div className="ChampionRecord-info-bottom">
+          <Typography
+            className="ChampionRecord-win-pct"
+            variant={TypographyVariants.p}
+            color={winPctColorRange.interpolate(winPct)}
+          >
+            {winPct}%
+          </Typography>
+          <Typography
+            className="ChampionRecord-win-count"
+            variant={TypographyVariants.xs}
+          >
+            {champRecord.win}W
+          </Typography>
+          <Typography
+            className="ChampionRecord-divider"
+            variant={TypographyVariants.xs}
+          >
+            /
+          </Typography>
+          <Typography
+            className="ChampionRecord-loss-count"
+            variant={TypographyVariants.xs}
+          >
+            {champRecord.loss}L
+          </Typography>
+        </div>
       </div>
     </div>
   );
